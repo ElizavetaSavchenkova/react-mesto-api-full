2ead341862c82de118423b80530c89fd36a2539f
Elizaveta Savchenkova <LizaRogers@yandex.ru>
2022-08-15 14:01:19 +0300

    fix:cors

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 
 const { validateCreateNewUser, validateUserLogin } = require('./middlewares/validate');
 const { createNewUser, login } = require('./controllers/users');
 const { errorVision } = require('./middlewares/errorVision');
+const cors = require('./middlewares/cors');
 
 const NotFoundError = require('./errors/NotFoundError');
 
-const app = express();
 const { PORT = 3000 } = process.env;
+const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cookieParser());
+app.use(cors);
 
 
 const routerUsers = require('./routes/users');
