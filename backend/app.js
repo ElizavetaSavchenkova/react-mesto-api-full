030414af99162ac58740f777e6c541587634ec2c
Elizaveta Savchenkova <LizaRogers@yandex.ru>
2022-08-15 13:06:03 +0300

    fix:cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
+
+const cookieParser = require('cookie-parser');
+const {requestLogger, errorLogger} = require('./middlewares/logger');
+
 const cors = require('./middlewares/cors');
 
 const { validateCreateNewUser, validateUserLogin } = require('./middlewares/validate');
@@ -12,18 +16,18 @@ const { errorVision } = require('./middlewares/errorVision');
 const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
+
+app.use(cors);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(requestLogger);
 
 
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
 
 app.post('/signup', validateCreateNewUser, createNewUser);
 app.post('/signin', validateUserLogin, login);
@@ -34,6 +38,11 @@ app.use((req, res, next) => {
   next(new NotFoundError('Указанная страница не найдена'));
 });
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use(errorLogger);
 app.use(errors());
 app.use(errorVision);
 
