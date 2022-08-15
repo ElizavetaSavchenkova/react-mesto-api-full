7ec37dfeaae52a7337e7dbaa961dd9c4d4e1a9b1
Elizaveta Savchenkova <LizaRogers@yandex.ru>
2022-08-15 14:32:00 +0300

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
+const cors = require('./middlewares/cors');
 
 const { validateCreateNewUser, validateUserLogin } = require('./middlewares/validate');
 const { createNewUser, login } = require('./controllers/users');
@@ -12,12 +13,13 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+app.use(cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
