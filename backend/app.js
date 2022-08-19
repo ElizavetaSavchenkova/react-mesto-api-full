1378917d0f9474244168eec1587ff82e72ddacfe
Elizaveta Savchenkova <LizaRogers@yandex.ru>
2022-08-19 19:16:24 +0300

    fix: dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+console.log(process.env);
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -14,7 +16,7 @@ const { validateCreateNewUser, validateUserLogin } = require('./middlewares/vali
 const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
